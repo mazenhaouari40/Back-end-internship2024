24a77d74ce7d2f053802b22f9c3a80c5bef95ae3
mazenhaouari40 <mazenhaouari97@gmail.com>
2024-07-24 15:35:38 +0200

    delete crossorigni

--- src/main/java/com/backend_jenkins/Backend/controller/UserController.java
+++ src/main/java/com/backend_jenkins/Backend/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://angular-jenkins.onrender.com/")
+//@CrossOrigin(origins = "https://angular-jenkins.onrender.com/")
 
 public class UserController {
     @Autowired
